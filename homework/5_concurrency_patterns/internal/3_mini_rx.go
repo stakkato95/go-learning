b47a361ea36cf0c103f9eb94fa3ffc89cb4209e6
ArtsiomKaliaha <stakkato95@gmail.com>
2021-09-11 18:09:31 +0200

    Add cancellation for rx example

--- homework/5_concurrency_patterns/internal/3_mini_rx.go
+++ homework/5_concurrency_patterns/internal/3_mini_rx.go
@@ -1,10 +1,18 @@
 package internal
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type observable struct {
-	in  chan int
-	out chan int
+	in   chan int
+	out  chan int
+	done chan interface{}
+}
+
+func newObservable() *observable {
+	return &observable{done: make(chan interface{})}
 }
 
 func (s observable) just(number, times int) observable {
@@ -18,12 +26,17 @@ func (s observable) just(number, times int) observable {
 
 		fmt.Println("just() started")
 		for i := 0; i < times; i++ {
-			fmt.Printf("%d just()\n", i+1)
-			s.out <- number
+			select {
+			case <-s.done:
+				return
+			case s.out <- number:
+				fmt.Printf("%d just()\n", i+1)
+				time.Sleep(1 * time.Second)
+			}
 		}
 	}()
 
-	return observable{in: s.out}
+	return observable{in: s.out, done: s.done}
 }
 
 func (s observable) mapping(m mapper) observable {
@@ -38,28 +51,48 @@ func (s observable) mapping(m mapper) observable {
 		fmt.Println("mapping() started")
 		var i int
 		for item := range s.in {
-			fmt.Printf("%d mapping()\n", i+1)
-			i++
-			s.out <- m(item)
+			select {
+			case <-s.done:
+				return
+			case s.out <- m(item):
+				fmt.Printf("%d mapping()\n", i+1)
+				i++
+				time.Sleep(1 * time.Second)
+			}
 		}
 	}()
 
-	return observable{in: s.out}
+	return observable{in: s.out, done: s.done}
 }
 
 func (s observable) printer() observable {
 	fmt.Println("printer() started")
 	var i int
 	for item := range s.in {
-		fmt.Printf("%d printer()\n", i+1)
-		i++
-		fmt.Println(item)
+		select {
+		case <-s.done:
+			fmt.Println("printer() cancelled")
+			return s
+		default:
+			fmt.Printf("%d printer() - %d\n", i+1, item)
+			i++
+			time.Sleep(1 * time.Second)
+		}
 	}
 	fmt.Println("printer() finished")
 	return s
 }
 
 func SimpleRxTest() {
-	var rx observable
+	rx := newObservable()
+	done := rx.done
+
+	go func() {
+		fmt.Println("pipeline will be cancelled in 7 sec...")
+		time.Sleep(7 * time.Second)
+		close(done)
+		fmt.Println("pipeline cancelled after 7 sec...")
+	}()
+
 	rx.just(33, 10).mapping(func(i int) int { return i * 3 }).printer()
 }
